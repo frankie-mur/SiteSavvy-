6fbd9c0193de844eb10502b453b10de39520a9a3
Frankie <frankidatank@gmail.com>
2023-05-05 11:28:16 -0700

    feat(ContactPage): Implement contact page and server to send email

---
+++ event-smart/src/ContactForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Form, Button } from 'react-bootstrap';
+
+const ContactForm = () => {
+    const [formData, setFormData] = useState({
+  name: '',
+  email: '',
+  subject: '',
+  message: ''
+    });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let response = await fetch("http://localhost:5001/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formData),
+        });
+        let result = await response.json();
+        alert(result.status);
+    };
+    return (
+        <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your name" value={formData.name} onChange={e => setFormData({ ...formData, name: e.target.value })} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter your email" value={formData.email} onChange={e => setFormData({ ...formData, email: e.target.value })} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicSubject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control type="text" placeholder="Enter a subject" value={formData.subject} onChange={e => setFormData({ ...formData, subject: e.target.value })} />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicMessage">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Enter your message" value={formData.message} onChange={e => setFormData({ ...formData, message: e.target.value })} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+            Submit
+        </Button>
+        </Form>
+        );
+};
+
+export default ContactForm;
