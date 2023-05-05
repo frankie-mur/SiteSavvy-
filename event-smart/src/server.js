8cdafa752f5e325d445ee3e0cf27960673e817e3
Frankie <frankidatank@gmail.com>
2023-05-05 11:45:57 -0700

    formatting

--- event-smart/src/server.js
+++ event-smart/src/server.js
@@ -3,50 +3,47 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5001, () => console.log("Server Running"));
 
-
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "........@gmail.com",
-        pass: "some_password",
-    },
+  service: "gmail",
+  auth: {
+    user: "........@gmail.com",
+    pass: "some_password",
+  },
 });
 
 contactEmail.verify((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Server up and running..");
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server up and running..");
+  }
 });
 
-
 router.post("/contact", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    const subject = req.body.subject
-    const mail = {
-        from: name,
-        to: "frankidatank@gmail.com",   
-        subject: "Contact Form Submission",
-        html: `<p>Name: ${name}</p>
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const subject = req.body.subject;
+  const mail = {
+    from: name,
+    to: "frankidatank@gmail.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Subject: ${subject}</p>
            <p>Message: ${message}</p>`,
-    };
-    contactEmail.sendMail(mail, (error) => {
-        if (error) {
-            res.json({ status: "ERROR" });
-        } else {
-            res.json({ status: "Message Sent" });
-        }
-    });
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: "ERROR" });
+    } else {
+      res.json({ status: "Message Sent" });
+    }
+  });
 });
